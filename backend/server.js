6ec7c9ee22e7d73f8fc9ce0a48d668578960cea8
PrashanthAgithe <156330705+PrashanthAgithe@users.noreply.github.com>
2025-11-15 00:10:00 +0530

    Done some changes

--- backend/server.js
+++ backend/server.js
@@ -44,7 +44,7 @@ app.get("/getallstudents", async (req, res) => {
   res.json(students);
 });
 
-// Get One Student by ID
+// Get One Student by rollno
 app.get("/getstudent/:rollno", async (req, res) => {
   try {
     const student = await Student.findOne({ rollno: req.params.rollno });
